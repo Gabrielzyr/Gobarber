e63775738353c2c0d7f85e85513f2dbafcc3d6de
GabrielZyr <64475947+Gabrielzyr@users.noreply.github.com>
2020-10-01 21:21:33 -0300

    Baes structure

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+app.use(express.json())
+app.use(routes)
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello jose' });
+});
+
+app.listen(3333, () => {
+  console.log('🐱‍🏍 Server started on port 3333!');
+});
