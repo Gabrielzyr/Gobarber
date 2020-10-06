b4b75f8964dbce95fc094c7a45bbeb13d13e9b1b
Gabrielzyr <gabrielmz397@gmail.com>
2020-10-06 12:48:39 -0300

    database migrations

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 const app = express();
-app.use(express.json())
-app.use(routes)
+app.use(express.json());
+app.use(routes);
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello jose' });
 });
