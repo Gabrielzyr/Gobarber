59de6201b8a988e0d2230bd633883e6de96256ad
Gabrielzyr <gabrielmz397@gmail.com>
2020-10-09 18:09:32 -0300

    Created global exception handler

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -6,21 +6,17 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user, token } = await authenticateUser.execute({
-      email,
-      password,
-    });
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
 
-    const mappedUser = UserMap.toDTO(user);
-    return response.json({ mappedUser, token });
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  const mappedUser = UserMap.toDTO(user);
+  return response.json({ mappedUser, token });
 });
 
 export default sessionsRouter;
