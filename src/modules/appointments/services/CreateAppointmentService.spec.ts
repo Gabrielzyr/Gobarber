d59e170d1e7dac6de0fda7ecf2a2da6f01bc216f
Gabrielzyr <gabrielmz397@gmail.com>
2020-12-04 11:51:11 -0300

    Added Create Appoitment Test

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,15 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should not be able to create a new appointment on the same time', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
