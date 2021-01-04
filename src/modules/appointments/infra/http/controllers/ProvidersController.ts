21568a8b790c4e6bca9e8f25cfc9ab93f96858e5
Gabrielzyr <gabrielmz397@gmail.com>
2021-01-04 10:17:37 -0300

    Added availability controllers

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 import { container } from 'tsyringe';
 
-export default class AppointmentsController {
+export default class ProviderController {
   public async index(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
 
